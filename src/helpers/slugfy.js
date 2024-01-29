f8f528580687d850dd319ffd22f14440f9ee92aa
Leonardo César <leo_cesa@hotmail.com>
2024-01-29 00:07:28 -0300

    fix: slugfy

--- src/helpers/slugfy.js
+++ src/helpers/slugfy.js
@@ -1,6 +1,11 @@
-export function slugify(str) {
-  return str
+export function slugfy(string) {
+  if (!string || typeof string !== "string") return "";
+
+  string = string
+    .trim()
     .toLowerCase()
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/^-|-$/g, "");
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  string = string.replace(/[ \t]+/g, "-");
+  return string;
 }
